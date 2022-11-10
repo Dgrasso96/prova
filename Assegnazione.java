333fd29cf0beb477dc702a597f3186d98b326480
DavideG96 <davidegrasso966@gmail.com>
2022-11-10 20:21:28 +0100

    first commit

---
+++ Assegnazione.java
@@ -0,0 +1,16 @@
+
+public class Assegnazione {
+	public static void main(String argv[]) {
+		int a = 10;
+		int b = 11;
+		int c;
+
+		c = a + b;
+		b = a * 2;
+
+		System.out.println(c);
+		System.out.println(b);
+		System.out.println(b + 1);
+	}
+
+}
